dde11a4040d09d91ea9c131f2d4addef82a62c10
unknown <your_emailexample.com>
2025-03-06 14:55:03 +0100

    final

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('togglebtn')
+const menu = document.querySelector('.menu')
+const close = document.getElementById('close')
+
+togglebtn.addEventListener('click', (e) => {
+menu.classList.toggle('open')
+menu.style.transition= "1.5s"
+
+})
+close.addEventListener('click', (e) => {
+    menu.classList.remove('open')
+})
+
